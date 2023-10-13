d15016856b13ac060894876cb1f1df2fbcf92740
Josaphat Campos <t3691281@timbrasil.com.br>
2023-10-13 13:58:35 -0300

    CRUD

--- lib/controllers/list_controller.dart
+++ lib/controllers/list_controller.dart
@@ -1,20 +1,57 @@
-
-import 'dart:math';
-
 import 'package:ebac_todolist/models/item.dart';
 import 'package:flutter/cupertino.dart';
 
-
-class ListController extends ValueNotifier<List<Item>>{
+class ListController extends ValueNotifier<List<Item>> {
   List<Item> list = [];
 
-  ListController() : super([]) ;
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
 
-  void addItem(){
-    int id = list.length + 1;
-    final item = Item(id: id, title: "item $id", description: 'decrição', isDone: false);
+  ListController() : super([]);
 
-    list.add(item);
+  void addItem(
+      {required String title, required String description, int id = 0}) {
+    if (title.isNotEmpty && description.isNotEmpty) {
+      final item = Item(
+          id: _generateid(id),
+          title: title,
+          description: description,
+          isDone: false);
+
+      insertEdit(item);
+    }
+    _clearControllers();
     notifyListeners();
+  }
+
+  insertEdit(Item item) {
+    final oldItem = list.where((element) => element.id == item.id).toList();
+
+    if (oldItem.isEmpty) {
+      list.add(item);
+    } else {
+      final index = list.indexOf(oldItem[0]);
+      list.removeAt(index);
+      list.insert(index, item);
+    }
+    notifyListeners();
+  }
+
+  void removeItem(int index) {
+    list.removeAt(index);
+    notifyListeners();
+  }
+
+  int _generateid(int id) {
+    if (id == 0) {
+      return list.length > 0 ? list.last.id + 1 : list.length + 1;
+    } else {
+      return id;
+    }
+  }
+
+  _clearControllers() {
+    titleController.clear();
+    descriptionController.clear();
   }
 }
--- lib/views/list_page.dart
+++ lib/views/list_page.dart
@@ -27,10 +27,81 @@ class _ListPageState extends State<ListPage> {
     _controller.dispose();
   }
 
+  Future<dynamic> callDialog(BuildContext context, {Item? item}) {
+    if (item != null) {
+      _controller.titleController.text = item!.title;
+      _controller.descriptionController.text = item!.description;
+    }
 
-  Future<dynamic> callDialog(BuildContext context, {Item? item}){
-
-    return Future(() => null);
+    return showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Center(child: Text("To Do")),
+          content: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: _controller.titleController,
+                decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.all(8),
+                    label: Text("Título"),
+                    hintText: 'Titulo',
+                    hintStyle:
+                        TextStyle(fontStyle: FontStyle.italic, fontSize: 12),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)))),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              TextField(
+                controller: _controller.descriptionController,
+                decoration: const InputDecoration(
+                    isDense: true,
+                    contentPadding: EdgeInsets.all(8),
+                    label: Text("Descrição"),
+                    hintText: 'Descrição',
+                    hintStyle:
+                        TextStyle(fontStyle: FontStyle.italic, fontSize: 12),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(8)))),
+              )
+            ],
+          ),
+          actions: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                    child: TextButton(
+                        onPressed: () {
+                          int id = item != null ? item!.id : 0;
+                          _controller.addItem(
+                              title: _controller.titleController.text,
+                              description:
+                                  _controller.descriptionController.text,
+                              id: id);
+                          Navigator.of(context).pop();
+                        },
+                        child: const Icon(
+                          Icons.check,
+                          color: Colors.red,
+                          size: 32,
+                        ))),
+              ],
+            )
+          ],
+          contentPadding: const EdgeInsets.all(16),
+          actionsOverflowButtonSpacing: 50,
+          titlePadding: const EdgeInsets.only(top: 32, bottom: 16),
+          titleTextStyle: const TextStyle(
+              color: Colors.red, fontWeight: FontWeight.bold, fontSize: 24),
+        );
+      },
+    );
   }
 
   @override
@@ -50,8 +121,19 @@ class _ListPageState extends State<ListPage> {
               return CheckboxListTile(
                 controlAffinity: ListTileControlAffinity.leading,
                 value: item.isDone,
-                title: Text(item.title),
-                subtitle: Text(item.description),
+                title: Text(
+                  item.title,
+                  style: TextStyle(
+                      decoration: item.isDone
+                          ? TextDecoration.lineThrough
+                          : TextDecoration.none,
+                      fontWeight: FontWeight.bold, color: item.isDone ? Colors.red:Colors.black),
+                ),
+                subtitle: Text(item.description,
+                    style: TextStyle(
+                        decoration: item.isDone
+                            ? TextDecoration.lineThrough
+                            : TextDecoration.none,  color: item.isDone ? Colors.red:Colors.black)),
                 secondary: Container(
                   width: 80,
                   child: Row(
@@ -69,20 +151,26 @@ class _ListPageState extends State<ListPage> {
                           color: Colors.red,
                         ),
                         onTap: () {
-                          debugPrint('delete $index');
+                          _controller.removeItem(index);
                         },
                       ),
                     ],
                   ),
                 ),
-                onChanged: (bool? value) {},
+                onChanged: (value) {
+                  _controller.insertEdit(Item(
+                      id: item.id,
+                      title: item.title,
+                      description: item.description,
+                      isDone: value ?? false));
+                },
               );
             },
           );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: (){
+        onPressed: () {
           callDialog(context);
         },
         tooltip: 'Increment',
